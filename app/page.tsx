d33da8961137ee2ce5a195454676a06d1930cab7
Suraj Kumar <surajsinhaking77@gmail.com>
2023-10-12 23:06:57 +0530

    done somechange

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,17 @@
 import Image from "next/image";
 
+
 export default function Home() {
   return (
     <main>
-      <div className="h-[100vh] w-auto bg-gradient-to-t  from-gray-800 to-red-600 flex flex-row items-center justify-center">
+      <div className="bg h-[100vh] w-auto flex flex-row items-center justify-center">
         <div className="mainHeading flex flex-col items-center">
-          <h1 className="text-[500%] text-white uppercase font-medium">
+          <h1 className="text-[500%] text-white uppercase font-medium font-['Autour_One']">
             Are you ready ?
           </h1>
 
-          <button className="cursor-pointer transition ease-in-out delay-150 hover:-translate-y-2 hover:scale-110 duration-100 text-red-600 text-[15rem] relative">
-            <span>X</span>
+          <button className="cursor-pointer transition ease-in-out delay-150 hover:-translate-y-2 hover:scale-110 duration-100 text-red-500 text-[15rem] relative x-for-font">
+            <span className="">X</span>
           </button>
         </div>
       </div>
